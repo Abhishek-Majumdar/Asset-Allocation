14e4667cc8a5c0a711588bff1d277718f78207c1
Hemanth Gandham <bgandham@ymail.com>
2018-08-27 05:20:57 -0700

    added check() function.
    Checks whether expected return rate(goal/net_worth) is achieved from the given risk appetite(vs return).

--- Asset Allocation/src/com/operations/Allocation.java
+++ Asset Allocation/src/com/operations/Allocation.java
@@ -10,23 +10,66 @@ import com.graph.PlotGraph;
 
 public class Allocation {
 
-	
-	public int[] allocate()
-	{
-       	PlotGraph p1 = new PlotGraph();
+	double []return_component=new double[3];
+	double []risk_component=new double[3];
+	double return_total,risk_appetite;
+	public Allocation() {
+		// TODO Auto-generated constructor stub
+		PlotGraph p1 = new PlotGraph();
        	List<Double> list = new ArrayList<>();
 		list = p1.plotGraph();
-		double return_total = list.get(0);
-		double risk_appetite = list.get(1);
+		return_total = list.get(0);
+		risk_appetite = list.get(1);
 		
-		double []return_component=new double[3];
-		double []risk_component=new double[3];
+		
 		for(int i=0;i<3;i++)
 		{
 			return_component[i]=p1.returns.get(i);
 			risk_component[i]=p1.risk.get(i);
 		}
-		
+	}
+	
+	public boolean check()
+	{
+		 ResultSet rs;
+	        DataValues d1 = new DataValues();
+	        int amount=0,principal=0;
+	        double expected_rate;
+	        rs = d1.fetchData("select AMT_OUT from CLIENT_GOAL");
+	        try
+	        {
+	        while(rs.next())
+	        {
+	        	amount=rs.getInt(1);
+	        }
+	        }catch(SQLException e) 
+	        {
+	        	e.printStackTrace();
+	        }
+	        
+	        rs = d1.fetchData("select CLIENT_NETWORTH from RISK_PROFILE");
+	        try
+	        {
+	        while(rs.next())
+	        {
+	        	principal=rs.getInt(1);
+	        }
+	        }catch(SQLException e) 
+	        {
+	        	e.printStackTrace();
+	        }
+	        
+	        expected_rate=((amount-principal)/principal)*100;
+	        
+	        if(expected_rate<return_total)
+	        return true;
+	        
+	        return false;
+	        		
+	}
+	
+	public int[] allocate()
+	{
 		MatrixOperations mo=new MatrixOperations();
 		double input[][]={ {1,1,1} , return_component , risk_component };
 		double product[][]={{1},{return_total},{risk_appetite}};
