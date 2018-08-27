b0280d66f8776246741e923e0feffed3f2a8ab0c
Hemanth Gandham <bgandham@ymail.com>
2018-08-27 04:18:02 -0700

    Updated Allocation.java to be compatible with PlotGraph.java

--- Asset Allocation/src/com/operations/Allocation.java
+++ Asset Allocation/src/com/operations/Allocation.java
@@ -10,18 +10,24 @@ import com.graph.PlotGraph;
 
 public class Allocation {
 
-	public int[] allocate(double [] return_component,double []risk_component)
+	
+	public int[] allocate()
 	{
-		double return_total; 
-		double risk_appetite;
-		
-       	MatrixOperations mo=new MatrixOperations();
+       	PlotGraph p1 = new PlotGraph();
        	List<Double> list = new ArrayList<>();
-		PlotGraph p1 = new PlotGraph();
 		list = p1.plotGraph();
-		return_total = list.get(0);
-		risk_appetite = list.get(1);
+		double return_total = list.get(0);
+		double risk_appetite = list.get(1);
 		
+		double []return_component=new double[3];
+		double []risk_component=new double[3];
+		for(int i=0;i<3;i++)
+		{
+			return_component[i]=p1.returns.get(i);
+			risk_component[i]=p1.risk.get(i);
+		}
+		
+		MatrixOperations mo=new MatrixOperations();
 		double input[][]={ {1,1,1} , return_component , risk_component };
 		double product[][]={{1},{return_total},{risk_appetite}};
 		double result[][]=mo.multiplyMatrices((mo.invertMatrix(input)), product);
@@ -30,7 +36,7 @@ public class Allocation {
 		percent[0]=(int)(result[0][0]*100);
 		percent[1]=(int)(result[1][0]*100);
 		percent[2]=100-percent[0]-percent[1];
-		System.out.println(percent[0]+","+percent[1]+","+percent[2]);
+		//System.out.println(percent[0]+","+percent[1]+","+percent[2]);
 		return percent;
 	}
 
