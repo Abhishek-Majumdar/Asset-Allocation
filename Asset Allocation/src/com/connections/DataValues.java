1f3addef5c98b2d76e8f8012370a1addb54133cc
Ira <iraparagnatu@gmail.com>
2018-08-28 10:32:22 +0530

    reverse_plotGraph

--- Asset Allocation/src/com/connections/DataValues.java
+++ Asset Allocation/src/com/connections/DataValues.java
@@ -54,19 +54,39 @@ public class DataValues {
 		}
 	}
 	
-	public static void addData(String query,int year,double val1,double val2, double val3, double val4, double val5, double val6) {
+	public static void addData(String query,String username,int year,double val1,double val2, double val3, double val4, double val5, double val6) {
 		
 		Connection conn = MyConnection.getMyConnection();
 		
 		try {
 			PreparedStatement ps = conn.prepareStatement(query);
 			ps.setInt(1, year);
-			ps.setDouble(2, val1);
-			ps.setDouble(3, val2);
-			ps.setDouble(4, val3);
-			ps.setDouble(5, val4);
-			ps.setDouble(6, val5);
-			ps.setDouble(7, val6);
+			ps.setString(2, username);
+			ps.setDouble(3, val1);
+			ps.setDouble(4, val2);
+			ps.setDouble(5, val3);
+			ps.setDouble(6, val4);
+			ps.setDouble(7, val5);
+			ps.setDouble(8, val6);
+			ps.executeUpdate(query);
+			
+		
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+public static void addData(String query,String val1,int val2,int val3, int val4) {
+		
+		Connection conn = MyConnection.getMyConnection();
+		
+		try {
+			PreparedStatement ps = conn.prepareStatement(query);
+			ps.setString(1, val1);
+			ps.setInt(2, val2);
+			ps.setInt(3, val3);
+			ps.setInt(4, val4);
 			ps.executeUpdate(query);
 			
 		
--- Asset Allocation/src/com/graph/PlotGraph.java
+++ Asset Allocation/src/com/graph/PlotGraph.java
@@ -29,7 +29,7 @@ public class PlotGraph {
         int random_max[]= {2,10,10};
         Randomizer random = new Randomizer();
         DataValues d1 = new DataValues();
-        rs = d1.fetchData("select * from market_data");
+        rs = d1.fetchData("SELECT * FROM MARKET_DATA");
         Min_max m1 = new Min_max();
         try {
 			while(rs.next()) {
@@ -64,7 +64,7 @@ public class PlotGraph {
         System.out.println(Arrays.toString(coeff));
         int risk_Score = 0;
          String username = null;
-        rs = d1.fetchData("select username from user_session where user_status=1");
+        rs = d1.fetchData("SELECT USERNAME FROM USER_SESSION WHERE USER_STATUS=1");
         try {
 			while(rs.next()) {
 				username = rs.getString(1);
@@ -73,7 +73,7 @@ public class PlotGraph {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        ResultSet rs1 = d1.fetchData("select risk_Score from risk_profile where username = ?",username);
+        ResultSet rs1 = d1.fetchData("SELECT RISK_SCORE FROM RISK_PROFILE WHERE USERNAME = ?",username);
         try {
 			while(rs1.next()) {
 				try {
@@ -101,4 +101,97 @@ public class PlotGraph {
         return_value.add((double) risk_Score);
         return return_value;
     }
+    
+    
+    
+    public ArrayList<Double> reverse_plotGraph() {
+        
+        ArrayList<Double> return_value = new ArrayList<Double>();
+
+        ResultSet rs;
+        Double  r_return;
+        int random_min[] = {-1,-10,-5};
+        int random_max[]= {2,10,10};
+        Randomizer random = new Randomizer();
+        DataValues d1 = new DataValues();
+        rs = d1.fetchData("SELECT * FROM MARKET_DATA");
+        Min_max m1 = new Min_max();
+        try {
+			while(rs.next()) {
+				risk.add(rs.getDouble(1));
+				returns.add(rs.getDouble(2));
+				
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+        
+        WeightedObservedPoints obs = new WeightedObservedPoints();
+        if(risk != null && risk.size() != 1) {
+            int size = risk.size();
+            for(int i = 0; i < size; i++) {
+                r_return = returns.get(i)*random.getRandomValue(random_min[i], random_max[i]);
+
+                    obs.add(r_return,risk.get(i));
+               
+            }
+        } 
+        
+//        obs.add(5,5);
+//        obs.add(7, 10);
+//        obs.add(10, 11);
+
+        PolynomialCurveFitter fitter = PolynomialCurveFitter.create(2);
+        //fitter.withStartPoint(new double[] {keyPoints.get(0), 1});
+        fitter.withStartPoint(new double[] {1,1});
+        double[] coeff = fitter.fit(obs.toList());
+        double networth = 0;
+        System.out.println(Arrays.toString(coeff));
+        double client_goal = 0;
+         String username = null;
+        rs = d1.fetchData("SELECT USERNAME FROM USER_SESSION WHERE USER_STATUS=1");
+        try {
+			while(rs.next()) {
+				username = rs.getString(1);
+			}
+			  ResultSet rs1 = d1.fetchData("SELECT AMT_OUT FROM CLIENT_GOAL WHERE USERNAME = ?",username);
+			  while(rs1.next()) {
+					
+						client_goal = rs1.getInt(1);
+					
+				}
+			  
+			  rs1 = d1.fetchData("SELECT CLIENT_NETWORTH FROM RISK_PROFILE WHERE USERNAME = ?",username);
+			  while(rs1.next()) {
+					
+						networth = rs1.getInt(1);
+					
+				}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        double expect_return ;
+        
+        expect_return = ((client_goal-networth)/networth)*100;
+        PolynomialFunction polynomial = new PolynomialFunction(coeff);
+        double suggested_risk_score =polynomial.value(expect_return);
+//        Double min, max;
+//        min = m1.find_min(risk);
+//        max = m1.find_max(risk);
+//        double risk_normal = min + ((risk_Score-1)/4*(max-min));
+//      
+    //    laguerreSolver.s
+     //   System.out.println("For x = 11, we found y = " + y);
+        return_value.add(expect_return);
+        return_value.add(suggested_risk_score);
+        return return_value;
+    }
 }
+
+
+
+/*
+CLIENT_NETWORTH - RISK_PROFILE
+*/
